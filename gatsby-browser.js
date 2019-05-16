86a255d6bc201fe2bea1b0ae67f60414311c7c6c
Johnny Magrippis <j@magrippis.com>
2019-05-16 16:24:15 +0100

    Reload if there is an update to the service worker, as it usually means new blog post or feature

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,7 @@ import 'typeface-eczar'
 import 'typeface-gentium-basic'
 // syntax highlighting
 import 'prismjs/themes/prism-okaidia.css'
+
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload()
+}
